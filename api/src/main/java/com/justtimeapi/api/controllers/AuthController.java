4b5998ccf3a3a840b15bebd31e101d8fe7bfc3a4
Iv44n <ivandev2oo6@gmail.com>
2025-10-11 18:10:13 -0500

    fix: update request mapping for AuthController and add Builder annotation to AppUser

--- api/src/main/java/com/justtimeapi/api/controllers/AuthController.java
+++ api/src/main/java/com/justtimeapi/api/controllers/AuthController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
--- api/src/main/java/com/justtimeapi/api/models/AppUser.java
+++ api/src/main/java/com/justtimeapi/api/models/AppUser.java
@@ -1,6 +1,7 @@
 package com.justtimeapi.api.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AppUser {
     private UUID id;
     private String username;
