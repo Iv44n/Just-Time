ca95cbb3a23e78d43b3712a2039571641576661d
Iv44n <ivandev2oo6@gmail.com>
2025-10-11 18:11:50 -0500

    feat: enhance JWT authentication filter and security configuration for role-based access control and CORS support

--- api/src/main/java/com/justtimeapi/api/security/JwtAuthFilter.java
+++ api/src/main/java/com/justtimeapi/api/security/JwtAuthFilter.java
@@ -1,10 +1,13 @@
 package com.justtimeapi.api.security;
 
 import com.justtimeapi.api.models.AppUser;
+import com.justtimeapi.api.repository.SessionRepository;
 import com.justtimeapi.api.repository.UserRepository;
+import com.justtimeapi.api.repository.UserRoleRepository;
 import com.justtimeapi.api.services.token.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
@@ -17,6 +20,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.UUID;
 
 @Component
@@ -25,35 +29,64 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    public static String getJwtFromRequest(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        Cookie[] cookies = request.getCookies();
+
+        boolean existsInHeader = (authHeader != null && authHeader.startsWith("Bearer "));
+        boolean existsInCookies = (cookies != null &&
+                Arrays.stream(cookies)
+                        .anyMatch(cookie -> cookie.getName().equals("accessToken")));
+
+        if (existsInHeader) {
+            return authHeader.substring(7);
+        } else if (existsInCookies) {
+            return Arrays.stream(cookies)
+                    .filter(cookie -> cookie.getName().equals("accessToken"))
+                    .findFirst()
+                    .map(Cookie::getValue)
+                    .orElse(null);
+        }
+        return null;
+    }
 
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
+        String jwt = getJwtFromRequest(request);
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (jwt == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        jwt = authHeader.substring(7);
-
         try {
             if (jwtService.isTokenValid(jwt) && jwtService.isAccessToken(jwt)) {
                 UUID userId = jwtService.extractUserId(jwt);
+                UUID sessionId = jwtService.extractSessionId(jwt);
 
-                if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                if (userId != null && sessionId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                     var userOpt = userRepository.findUserById(userId);
+                    var sessionOpt = sessionRepository.findSessionById(sessionId);
 
-                    if (userOpt.isPresent()) {
+                    if (userOpt.isPresent() && sessionOpt.isPresent()) {
                         AppUser user = userOpt.get();
+
+                        var roles = userRoleRepository.findRolesByUserId(user.getId());
+
+                        var authorities = roles.stream()
+                                .map(org.springframework.security.core.authority.SimpleGrantedAuthority::new)
+                                .toList();
 
                         UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                                 .username(user.getEmail())
                                 .password(user.getPassword())
+                                .authorities(authorities)
                                 .build();
 
                         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
--- api/src/main/java/com/justtimeapi/api/security/SecurityConfig.java
+++ api/src/main/java/com/justtimeapi/api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.justtimeapi.api.security;
 
 import com.justtimeapi.api.exceptions.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,11 +11,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Value("${client.allowed.origin}")
+    private String clientOrigin;
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAuthFilter jwtAuthFilter;
@@ -22,16 +30,33 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login", "/auth/refresh-token").permitAll()
-                        .requestMatchers("/auth/logout").authenticated()
+                        .requestMatchers("api/auth/register", "api/auth/login", "api/auth/refresh-token").permitAll()
+                        .requestMatchers("api/auth/logout").authenticated()
+                        .requestMatchers("api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(List.of(clientOrigin));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
